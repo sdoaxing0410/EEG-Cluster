5a71fad5be141dc6e9dbb24eba154cc5ec4a4748
Chivox <chivox@ChivoxdeMac-mini.local>
2019-12-17 17:29:04 +0800

    commit

---
+++ data/config.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+# data file path
+# MAHNOB-HCI
+MAHNOB_EEG_emotion_category_file_path = './data/MAHNOB-HCI/EEG_emotion_category.txt'
+MAHNOB_EEG_feature_file_path = './data/MAHNOB-HCI/EEG_feature.txt'
+MAHNOB_subject_video_file_path = './data/MAHNOB-HCI/subject_video.txt'
+MAHNOB_valence_arousal_label_file_path = './data/MAHNOB-HCI/valence_arousal_label.txt'
+
+# DEAP
+DEAP_EEG_feature_file_path = './data/DEAP/EEG_feature.txt'
+DEAP_subject_video_file_path = './data/DEAP/subject_video.txt'
+DEAP_valence_arousal_label_file_path = './data/DEAP/valence_arousal_label.txt'
+
+
+
---
+++ data/data_view.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from config import *
+import matplotlib.pyplot as plt
+import mne
+
+
+
